8c5a605b2aefe4426bac6410ec77250505a9277a
chisomdaniel <chinwezechisom@gmail.com>
2025-04-08 13:00:27 +0100

    added apps to installed app

--- musical_app_backend/settings.py
+++ musical_app_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
+    'apps.authuser',
+    'apps.userprofile',
 ]
 
 MIDDLEWARE = [
